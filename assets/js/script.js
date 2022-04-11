08d77354bbe5bede38ead6a55e7db363d0d746d8
Chrisborer <chris.borer@gmail.com>
2022-04-11 15:38:35 -0500

    unable to get anything to the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
 //Java script created by Christopher Borer
 //April 9th 2022.
+// node -r dotenv/config index.js;
 
 //variables
-var city = "Minneapolis";
-var getCurrentWeatherUrl="https://api.openweathermap.org/data/2.5/weather?q=Minneapolis&appid=0c390c97a57230e6547b396d84ff33a8";
+var apiKey = "0c390c97a57230e6547b396d84ff33a8";
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 //Elements
 //Data
+
 //function to get city input from user.
+$("#searchBtn").submit(function() {
+  var city = $("#currrentSearch").val();
+  console.log(city);
+  //Call to geet the current weather
+  getCurrentWeather(city);
+});
 
 //function to fetch data from weather api.
-var getCurrentWeather = function () {
-  //fetch(getCurrentWeatherUrl)
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=Minneapolis&appid=0c390c97a57230e6547b396d84ff33a8')
+var getCurrentWeather = function (city) {
+  let getCurrentWeatherUrl = apiUrl + "?q=" + city + "&appid=" + apiKey;
+  console.log(getCurrentWeatherUrl);
+  fetch(getCurrentWeatherUrl)
     .then((response) => {
       if (response.status >= 200 && response.status <= 299) {
         return response.json();
@@ -21,8 +30,9 @@ var getCurrentWeather = function () {
     })
     .then((response) => {
       // do whatever you want with the JSON response
-      .then(response) => console.log(response.statusText);
-    }).catch((error) => {
+      console.log(response);
+    })
+    .catch((error) => {
       // Handle the error
       console.log(error);
     });
@@ -31,4 +41,3 @@ var getCurrentWeather = function () {
 //funciton fetch 5 day forcast from api
 
 //function calls
-getCurrentWeather(city);
